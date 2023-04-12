553f03b3cc730c7daf05d88fed8fee610112b6a1
Gabriel Rozanczyk Ponzio <110409807+WoLF-Ponzio@users.noreply.github.com>
2023-04-11 21:44:14 -0300

    Correção pastas

---
+++ Atividade_01/src/Pessoa.java
@@ -0,0 +1,42 @@
+package Atividade_01.src;
+public abstract class Pessoa{
+
+    private String nome;
+    private String cpf;
+
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public void setCpf(String cpf){
+        // Remove os caractéres não numéricos do CPF
+        cpf = cpf.replaceAll("\\D", "");
+
+        // Formata o CPF com pontos e traço
+        cpf = cpf.substring(0, 3) + "." +
+              cpf.substring(3, 6) + "." +
+              cpf.substring(6, 9) + "-" +
+              cpf.substring(9, 11);
+        
+        this.cpf = cpf;      
+    }
+
+    @Override
+    public String toString() {
+        String aux = "";
+
+        aux += "Nome: " + getNome() + "\n";
+        aux += "CPF: " + getCpf() + "\n";
+        return aux;
+    }
+
+}
