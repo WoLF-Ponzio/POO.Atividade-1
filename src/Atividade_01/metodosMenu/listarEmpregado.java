5a70ee89d480d9824f7841fb387e6423e1e2e053
Andre Kalili <99913120+AndreKalili@users.noreply.github.com>
2023-04-13 20:38:28 -0300

    Listar Empregado criado!

--- src/Atividade_01/metodosMenu/listarEmpregado.java
+++ src/Atividade_01/metodosMenu/listarEmpregado.java
@@ -1,5 +1,30 @@
 package src.Atividade_01.metodosMenu;
 
+import java.util.ArrayList;
+
+import static javax.swing.JOptionPane.*;
+
+import src.Atividade_01.Classes.Empregado;
+import src.Atividade_01.Classes.Pessoa;
+
 public class listarEmpregado {
-    
+
+    public void listaEmp(ArrayList<Pessoa> listaPessoas) {
+        StringBuffer empregado = new StringBuffer();
+        for (Pessoa pessoa : listaPessoas) {
+            if (pessoa instanceof Empregado) {
+                empregado.append(pessoa.toString());
+            } else {
+
+            }
+        }
+        if (empregado.length() > 0) {
+            showConfirmDialog(null, "Listando Empregados\n" + empregado.toString(), "Lista de Empregados", DEFAULT_OPTION,
+                    INFORMATION_MESSAGE);
+        } else {
+            showConfirmDialog(null, "Não há empregados cadastrados até o momento", "Erro!", DEFAULT_OPTION,
+                    ERROR_MESSAGE);
+        }
+
+    }
 }
