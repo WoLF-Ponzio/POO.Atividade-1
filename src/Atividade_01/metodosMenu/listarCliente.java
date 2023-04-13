037bb6dc4954b83cbd8cd8e9ca7f32010a2bd9bf
V125-dev <110831331+ViniciusSMLuz@users.noreply.github.com>
2023-04-13 20:38:30 -0300

    criação do listar clientes

--- src/Atividade_01/metodosMenu/listarCliente.java
+++ src/Atividade_01/metodosMenu/listarCliente.java
@@ -4,18 +4,26 @@ import java.util.ArrayList;
 
 import static javax.swing.JOptionPane.*;
 
+import src.Atividade_01.Classes.Cliente;
 import src.Atividade_01.Classes.Pessoa;
 
 public class listarCliente {
 
     public void listaCli(ArrayList<Pessoa> listaPessoas) {
+        StringBuffer clientes = new StringBuffer();
+        for (Pessoa pessoa : listaPessoas) {
+            if (pessoa instanceof Cliente) {
+                clientes.append(pessoa.toString());
+            } else {
 
-        if (mensagem.equals("")) {
-            showConfirmDialog(null, "Clientes não encontrados");
-
+            }
+        }
+        if (clientes.length() > 0) {
+            showConfirmDialog(null, "Listando Clientes\n" + clientes.toString(), "Lista de Clientes", DEFAULT_OPTION,
+                    INFORMATION_MESSAGE);
         } else {
-            mensagem = "Cliente " + mensagem;
-            System.out.println(mensagem);
+            showConfirmDialog(null, "Não há clientes cadastrados até o momento", "Erro!", DEFAULT_OPTION,
+                    ERROR_MESSAGE);
         }
 
     }
