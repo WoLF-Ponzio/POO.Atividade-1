b43ea8242079fd7939ab621000cabcee40633307
Andre Kalili <99913120+AndreKalili@users.noreply.github.com>
2023-04-13 19:40:35 -0300

    Cadastro do Cliente feito!

--- src/Atividade_01/Classes/Cliente.java
+++ src/Atividade_01/Classes/Cliente.java
@@ -1,21 +1,26 @@
 package src.Atividade_01.Classes;
 
-public class Cliente extends Pessoa{
-    
+public class Cliente extends Pessoa {
+
     private double valorDaDivida;
 
-    public Cliente(String id, String nome, String cpf, double valorDaDivida){
-        //super(nome, cpf);
+    public Cliente(String id, String nome, String cpf, double valorDaDivida) {
+        // super(nome, cpf);
 
         this.valorDaDivida = valorDaDivida;
     }
 
-    public double getValorDaDivida(){
+    public Cliente() {
+        super();
+
+    }
+
+    public double getValorDaDivida() {
         return valorDaDivida;
     }
 
-    public void setValorDaDivida(double valorDaDivida){
+    public void setValorDaDivida(double valorDaDivida) {
         this.valorDaDivida = valorDaDivida;
     }
-    
+
 }
