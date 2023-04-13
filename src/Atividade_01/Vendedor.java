f12cdbf623dbca4655a628553a66fd1dfc55dcb7
tata28t <110830624+tata28t@users.noreply.github.com>
2023-04-12 21:33:25 -0300

    Criação Vendedor

---
+++ src/Atividade_01/Vendedor.java
@@ -0,0 +1,21 @@
+package src.Atividade_01;
+
+public class Vendedor extends Empregado {
+    
+    private double totaldasvendas;
+    private double comissao;
+    
+    public Vendedor (String matricula, double totaldasvendas, double comissao){
+
+        this.totaldasvendas = totaldasvendas;
+        this.comissao = comissao;
+
+    }
+
+
+    
+
+
+
+
+}
