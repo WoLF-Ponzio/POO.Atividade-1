d5c94c040600cfd543f1f57bce5cfd8b1b42d885
Gabriel Rozanczyk Ponzio <110409807+WoLF-Ponzio@users.noreply.github.com>
2023-04-13 19:13:35 -0300

    Fucionamento do cadastro do Empregado Gerente

--- src/Atividade_01/metodosMenu/cadastrarEmpregado.java
+++ src/Atividade_01/metodosMenu/cadastrarEmpregado.java
@@ -1,38 +1,58 @@
 package src.Atividade_01.metodosMenu;
+
 import src.Atividade_01.Classes.*;
 import static javax.swing.JOptionPane.*;
+import java.util.ArrayList;
 import static java.lang.Integer.parseInt;
 import static java.lang.Double.parseDouble;
 
 public class cadastrarEmpregado {
 
-    public void CadastrarEmpregado() {
+    public Pessoa CadastrarEmpregado(ArrayList<Pessoa> listaPessoas) {
+        // Pega o arraylist para verificar se já existe algum funcionário com o mesmo
+        // CPF já cadastrado
         int menu;
+        boolean teste = true;
 
         try {
-
-            menu = parseInt(showInputDialog(null, "1 - Gerente\n2 - Vendedor", "Digite qual o tipo de funcionário",
-                    DEFAULT_OPTION));
+            menu = parseInt(showInputDialog(null, "1 - Gerente\n2 - Vendedor", "Digite qual o tipo de funcionário",DEFAULT_OPTION));
             if (menu == 1) {
                 Gerente gerente = new Gerente();
-                gerente.setNome(showInputDialog(getRootFrame(), "Digite o nome do Gerente", "Cadastro do Gerente", DEFAULT_OPTION));
-                gerente.setCpf(showInputDialog(getRootFrame(), "Digite o CPF do Gerente", "Cadastro do Gerente", DEFAULT_OPTION));
-                gerente.setBonus(parseDouble(showInputDialog(getRootFrame(), "Digite o Bonûs salarial", "Cadastro do Gerente", DEFAULT_OPTION)));
-                gerente.setSalario(parseDouble(showInputDialog(getRootFrame(), "Digite o Salário", "Cadastro do Gerente", DEFAULT_OPTION)));
+                gerente.setNome(showInputDialog(getRootFrame(), "Digite o nome do Gerente", "Cadastro do Gerente",DEFAULT_OPTION));
+                while (teste == true) {
+                    gerente.setCpf(showInputDialog(getRootFrame(), "Digite o CPF do Gerente", "Cadastro do Gerente", DEFAULT_OPTION));
+                    teste = false;
+                    if(listaPessoas.isEmpty()){
+                        teste = false;
+                    }else{
+                        for (Pessoa pessoa : listaPessoas) {
+                            if (pessoa.getCpf().equals(gerente.getCpf())) {
+                                showConfirmDialog(null, "CPF já cadastrado! Digite um CPF válido!", "Erro!", DEFAULT_OPTION, ERROR_MESSAGE);
+                                teste = true;
+                            } else {
+                                
+                            }
+                        }
+                    }
+                }
+                gerente.setBonus(parseDouble(showInputDialog(null, "Digite o Bonûs salarial", "Cadastro do Gerente", DEFAULT_OPTION)));
+                gerente.setSalario(parseDouble(showInputDialog(null, "Digite o Salário", "Cadastro do Gerente", DEFAULT_OPTION)));
                 System.out.println(gerente);
-            
-            } else if (menu == 2) {
+                return gerente;
 
+            } else if (menu == 2) {
+                // cadastro do vendedor
+                return null;
             } else {
-                showConfirmDialog(null, "Entrada de dado inválida! Repita o processo novamente", "Erro!",
-                        DEFAULT_OPTION, ERROR_MESSAGE);
-                return;
+                showConfirmDialog(null, "Entrada de dado inválida! Repita o processo novamente", "Erro!", DEFAULT_OPTION, ERROR_MESSAGE);
+                return null;
             }
 
         } catch (Exception e) {
             showConfirmDialog(null, "Entrada de dado inválida! Repita o processo novamente", "Erro!", DEFAULT_OPTION,
                     ERROR_MESSAGE);
         }
+        return null;
 
     }
 
