ca279a55722dc43ef41581c4f9380b33871a5cae
Andre Kalili <99913120+AndreKalili@users.noreply.github.com>
2023-04-12 23:38:52 -0300

    switch case com metodos menu

--- src/Atividade_01/Menu.java
+++ src/Atividade_01/Menu.java
@@ -1,22 +1,58 @@
 package src.Atividade_01;
+
 import static javax.swing.JOptionPane.*;
 
 import static java.lang.Integer.*;
 
-public class Menu{
+public class Menu {
 
-    public void menu(){
+    public void menu() {
         int menu = 0;
 
-        do{
+        do {
             try {
-            
-                menu = parseInt(showInputDialog(null, "1 - Cadastrar Empregado\n2 - Cadastrar Cliente\n3 - Pesquisar\n4 - Listar Empregados\n5 - Listar Clientes\n6 - Remover\n7 - Finalizar", "Menu Principal", INFORMATION_MESSAGE));
-            
+
+                menu = parseInt(showInputDialog(null,
+                        "1 - Cadastrar Empregado\n2 - Cadastrar Cliente\n3 - Pesquisar\n4 - Listar Empregados\n5 - Listar Clientes\n6 - Remover\n7 - Finalizar",
+                        "Menu Principal", INFORMATION_MESSAGE));
+
+                switch (menu) {
+                    case 1:
+
+                    CadastrarEmpregado();
+
+                        break;
+                    case 2:
+
+                    CadastrarCliente();
+
+                        break;
+                    case 3:
+
+                    Pesquisar();
+
+                        break;
+                    case 4:
+
+                    ListarEmpregado();
+
+                        break;
+                    case 5:
+
+                    ListarCliente();
+
+                        break;
+                    case 6:
+
+                    Remover();
+
+                        break;
+                }
+
             } catch (Exception e) {
-              showConfirmDialog(null, "Entrada de dado inválida!", "Erro!", DEFAULT_OPTION, ERROR_MESSAGE);
+                showConfirmDialog(null, "Entrada de dado inválida!", "Erro!", DEFAULT_OPTION, ERROR_MESSAGE);
             }
-        }while(menu != 7);   
-            
+        } while (menu != 7);
+
     }
 }
