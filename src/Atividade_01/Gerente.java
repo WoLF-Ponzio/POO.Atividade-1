55d7c931c47ff6df57536a67bc8a474d4ff0605e
EttoreENC <99734573+EttoreENC@users.noreply.github.com>
2023-04-12 21:43:41 -0300

    trabalho manual

---
+++ src/Atividade_01/Gerente.java
@@ -0,0 +1,15 @@
+package src.Atividade_01;
+
+public class Gerente extends Empregado {
+
+    private double bonus;
+    private double salario;
+
+    public Gerente(String id, String nome, String cpf, double salario, double bonus){
+        //super(nome, cpf);
+
+        this.bonus = bonus;
+        this.salario = salario;
+    }
+
+}
---
+++ src/Atividade_01/ListarEmpregado.java
@@ -0,0 +1,5 @@
+package src.Atividade_01;
+
+public class ListarEmpregado {
+    
+}
