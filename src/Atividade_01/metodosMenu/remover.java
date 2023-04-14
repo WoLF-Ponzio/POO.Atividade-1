738ff0fdf81def5bcd1f229517fc9ea930c18c8d
Andre Kalili <99913120+AndreKalili@users.noreply.github.com>
2023-04-13 23:10:43 -0300

    Ajustes no Remover

--- src/Atividade_01/metodosMenu/remover.java
+++ src/Atividade_01/metodosMenu/remover.java
@@ -6,10 +6,12 @@ import static javax.swing.JOptionPane.*;
 import src.Atividade_01.Classes.Cliente;
 import src.Atividade_01.Classes.Empregado;
 import src.Atividade_01.Classes.Pessoa;
+import javax.swing.JOptionPane;
 
 public class remover {
 
     public void removerP(ArrayList<Pessoa> dababy) {
+
         Pessoa pesquisarCpf = new Pessoa();
         pesquisarCpf.setCpf(showInputDialog("Digite o CPF que deseja pesquisar"));
 
@@ -17,22 +19,36 @@ public class remover {
         for (Pessoa pessoa : dababy) {
             if (pessoa.getCpf().equals(pesquisarCpf.getCpf())) {
                 pessoaEncontrada = pessoa;
-                
-                Pessoa pessoacpf = null;
-                
-                pessoacpf = new Pessoa("", pessoa.getCpf());
-
-                dababy.remove(pessoacpf);
-
                 break;
             }
-        }
-        if (pessoaEncontrada == null) {
-            showConfirmDialog(null, "Pessoa não encontrada", "Pesquisa", DEFAULT_OPTION, ERROR_MESSAGE);
-        } else {
 
         }
-        
+
+        if (pessoaEncontrada == null) {
+            showConfirmDialog(null, "Pessoa não encontrada", "Pesquisa", DEFAULT_OPTION, ERROR_MESSAGE);
+            return;
+        } else {
+
+            int escolha = JOptionPane.showOptionDialog(null, "Deseja deletar " + pessoaEncontrada.getNome() + " ?",
+                    "Confirmação de Exclusão",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+            if (escolha == JOptionPane.YES_OPTION) {
+                dababy.remove(pessoaEncontrada);
+
+                JOptionPane.showMessageDialog(null, "Pessoa excluida com sucesso");
+
+                return;
+            } else if (escolha == JOptionPane.NO_OPTION) {
+                JOptionPane.showMessageDialog(null, "Cancelando exclusão...");
+
+                return;
+            } else if (escolha == JOptionPane.CLOSED_OPTION) {
+                JOptionPane.showMessageDialog(null, "Caixa de diálogo fechada sem escolha...");
+
+                return;
+            }
+        }
     }
 
 }
